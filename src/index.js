e113623aa07a01a7e649ee446cef75b06d8773cc
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-12-12 17:49:56 -0500

    Add header tabs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hey does this work lol');
+import './styles.css';
+
+const homeBtn = document.querySelector('.home');
+const menuBtn = document.querySelector('.menu');
+const contactBtn = document.querySelector('.contact');
+
+homeBtn.addEventListener('click', () => {
+    homeBtn.classList.add('selected');
+    menuBtn.classList.remove('selected');
+    contactBtn.classList.remove('selected');
+});
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.add('selected');
+    homeBtn.classList.remove('selected');
+    contactBtn.classList.remove('selected');
+});
+
+contactBtn.addEventListener('click', () => {
+    contactBtn.classList.add('selected');
+    homeBtn.classList.remove('selected');
+    menuBtn.classList.remove('selected');
+});
